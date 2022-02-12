f38a7f071863e43f39f8390b1ae1a836a683afc0
Adel_ <Adel_@DESKTOP-64HRO07>
2022-02-12 12:59:11 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    
+    public class HomeController : Controller
+    {
+        [HttpGet("getnumber")]
+        public int calculator(int a , int b)
+        {
+            int c = a + b;
+            return c;
+        }
+
+    }
+}
